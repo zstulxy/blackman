050bb18f4935b998395782d81fb012f6d06c5da7
Your Name <you@example.com>
2018-07-04 23:07:18 +0800

    mybatis generator 生成是不生成example

--- src/main/java/com/maiya/dao/UserTblMapper.java
+++ src/main/java/com/maiya/dao/UserTblMapper.java
@@ -1,32 +1,15 @@
 package com.maiya.dao;
 
 import com.maiya.bean.UserTbl;
-import com.maiya.bean.UserTblExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface UserTblMapper {
-    int countByExample(UserTblExample example);
-
-    int deleteByExample(UserTblExample example);
-
     int deleteByPrimaryKey(Long id);
 
     int insert(UserTbl record);
 
     int insertSelective(UserTbl record);
 
-    List<UserTbl> selectByExampleWithBLOBs(UserTblExample example);
-
-    List<UserTbl> selectByExample(UserTblExample example);
-
     UserTbl selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") UserTbl record, @Param("example") UserTblExample example);
-
-    int updateByExampleWithBLOBs(@Param("record") UserTbl record, @Param("example") UserTblExample example);
-
-    int updateByExample(@Param("record") UserTbl record, @Param("example") UserTblExample example);
 
     int updateByPrimaryKeySelective(UserTbl record);
 
